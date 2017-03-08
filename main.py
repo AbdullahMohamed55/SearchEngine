bfdb9ee9e071cc2681d556443e7b23c471519266
ZidanMusk <mahmoud.ismail.zidan@gmail.com>
2017-03-09 01:42:50 +0200

    main to be...

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import Crawler
+from Model import *
+from Crawler import Crawler
+from Indexer import Indexer
+
 
 SEED_PATH = 'seeds.txt'
 DOWNLOAD_PATH = './pages/'
 
-crawlerObj = Crawler.Crawler(SEED_PATH, DOWNLOAD_PATH)
-crawlerObj.start()
+#crawlerObj = Crawler(SEED_PATH, DOWNLOAD_PATH)
+#crawlerObj.start()
+
+DB.connect()
+
+"""RUN this only once when creating tables!"""
+#sDB.create_tables([IndexerTable,UnCrawledTable,CrawledTable])
+
+"""our engine starts here"""
+
+DB.close()
