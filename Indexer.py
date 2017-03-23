5e26ed184b0b70bce7c24fbfb136c7266f9441bd
ZidanMusk <mahmoud.ismail.zidan@gmail.com>
2017-03-24 01:37:13 +0200

    indexing speed boost

--- Indexer.py
+++ Indexer.py
@@ -14,45 +14,47 @@ class Indexer:
 
         #delete old entries of this url (if exists any)
         self._deleteOldEntries(url)
+        bulkEntries = []
 
-        #parse the webpage content
+        #parse the web page content
         wordIndexes, wordImportance = self.parser(content)
 
+        #insert page extracted words in a list for bulk db insertions
         for word in wordImportance:
             if (word in wordIndexes):
-                print(word, wordIndexes[word], wordImportance[word])
-                print(self._addToIndex(word, url, wordIndexes[word], wordImportance[word]))
+                bulkEntries.append({'keyword' : word ,'url': url, 'positions' : wordIndexes[word], 'importance':wordImportance[word]})
 
+        #insert new entries in IndexerTable...Fastest way!
+        with DB.atomic():
+            #sqlite max vars = 999 per bulk insertion
+            size = (999 // len(bulkEntries[0]))
+            for i in range(0, len(bulkEntries), size):
+                IndexerTable.insert_many(bulkEntries[i:i + size]).upsert().execute()
+        print("Inserted:",len(bulkEntries),"new entries for url:",url)
 
 
     def _deleteOldEntries(self,url):
 
         query = IndexerTable.delete().where(IndexerTable.url == url)
-        print("deleted ", query.execute(), "entries.")
+        print("Deleted:", query.execute(), "old entries for url:",url)
 
 
-    # inserts a new entry(keyword,url)
+    '''inserts a new entry(keyword,url)'''
     def _addToIndex(self, keyword, url, pos = [], importance = 2):
 
         try:
-
             IndexerTable.create(keyword= keyword , url = url, positions = pos, importance = importance).update()
 
         except IntegrityError: # keyword & url already exists!
-
             return -1
 
         return 1
 
-    # returns dic[keyword]->[urls] of searched word(s) or -1
-    pass
 
-    #for searching
-    '''IN: list of keywords OUT: dict[keyword] of urls'''
+    '''Searching with words...IN: list of keywords, OUT: dict[keyword] of urls'''
     def lookupWithWords(self, keywords = []):
 
         result = {}
-
         for word in keywords:
             urlList = []
             try:
@@ -68,11 +70,10 @@ class Indexer:
         return result
 
 
-    '''IN: list of urls OUT: dict[url] of keywords'''
+    '''getting keywords with urls...IN: list of urls, OUT: dict[url] of keywords'''
     def lookupWithPages(self, pages=[]):
 
         result = {}
-
         for pg in pages:
             wordList = []
             try:
@@ -87,12 +88,13 @@ class Indexer:
 
         return result
 
-    '''Parsing functions'''
 
+    '''-----------------------------------Parsing functions----------------------------------------------------------'''
+
+    '''IN:[word1, word2, ...], OUT:{word1: [pos1, pos2], word2: [pos2, pos3], ...}'''
     def _indexFile(self,wordList):
+
         fileIndex = {}
-        # input = [word1, word2, ...]
-        # output = {word1: [pos1, pos2], word2: [pos2, pos3], ...}
         for index, word in enumerate(wordList):
             if word in fileIndex.keys():
                 fileIndex[word].append(index)
@@ -108,19 +110,20 @@ class Indexer:
                 importanceMap[word]= imp
                 count+=1
 
-    def _visibleText(self,element):  # removing text in script and comment
+    '''removes text in scripts and comments'''
+    def _visibleText(self,element):
 
         if element.parent.name in ['style', 'script', 'link', '[document]']:
             return False
-
         elif re.match('.*<!--.*-->.*', str(element)):
             return False
+
         return True
+
 
     def _parseKeywords(self,text):
 
         for i in range(len(text)):
-
             pattern = re.compile('[\W_]+')
             text[i] = pattern.sub(' ', text[i])
             re.sub(r'[\W_]+', '', text[i])
@@ -129,6 +132,7 @@ class Indexer:
             text[i] = None
 
         return text
+
 
     def _removeStopWords(self,wordList):
 
@@ -139,7 +143,6 @@ class Indexer:
 
     def _initParser(self,text):
 
-
         soup = BeautifulSoup(text, 'html.parser')
 
         # ---------------Finding Comments to remove them---------------
@@ -149,28 +152,20 @@ class Indexer:
         # ---------------Getting Text without Comments-------------------
         texts = soup.find_all(text=True)
 
-
         # ---------------removing the unwanted script and links from the doc and returning list of words------------
         visible_texts = filter(self._visibleText, texts)
 
         # ---------------remove all symbols like #$%@ , spaces and newlines--------------
-
         parsedWords = self._parseKeywords(list(visible_texts))
         parsedWords = [word for word in parsedWords if word is not ' ' and word is not None]
         # remove stop words
         parsedWords = self._removeStopWords(parsedWords)
 
-        # stemming words
-        pass #NOW??
-        '''
-        stemmer = PorterStemmer()
-        for i in range(len(parsedWords)):
-            parsedWords[i] = stemmer.stem(parsedWords[i])
-        '''
+
         return parsedWords
 
-    def parser(self, htmlDoc): #this function works on a html doc
-
+    '''parsing an html doc'''
+    def parser(self, htmlDoc):
 
         soup = BeautifulSoup(htmlDoc, 'html.parser')
         count = 0
@@ -190,13 +185,11 @@ class Indexer:
         headers = self._initParser(' '.join(headers))
         self._assignImportance(headers, count, 1, importMap)
 
-
         #---------------------parsing  plain text---------------------
         plainText = self._initParser(htmlDoc)
         self._assignImportance(plainText,count,2,importMap)
 
         # --------------indexing words -------------
         indexMap = self._indexFile(plainText)
-
 
         return indexMap,importMap
