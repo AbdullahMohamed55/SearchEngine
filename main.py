c4d25e431fa61602329289f1f8dd8906db77c603
Ahmed <ahmedossama20@gmail.com>
2017-03-23 02:42:07 +0200

    Crawler fully working.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Model import *
 from Crawler import Crawler
 from Indexer import Indexer
 from datetime import *
+import threading
+
 import os
 
 DB.connect()
@@ -12,7 +14,23 @@ if not DB.get_tables():
     Seeds(pageURL = 'https://www.reddit.com', crawlFrequency = 0, lastCrawl = datetime(1960, 1, 1, 1, 1, 1)).save()
 
 """our engine starts here"""
-crawlerObj = Crawler()
-crawlerObj.start()
+
+
+numberOfThreads = input('Enter number of threads: ')
+while not numberOfThreads.isnumeric():
+    numberOfThreads = input('Invalid input, please enter a valid number: ')
+
+crawlerObjs = []
+
+print('Creating ' + numberOfThreads + ' crawler objects.')
+for i in range (int(numberOfThreads)):
+    crawlerObjs.append(Crawler(i))
+
+for i in range (len(crawlerObjs)):
+    crawlerObjs[i].start()
+
+for i in range (len(crawlerObjs)):
+    crawlerObjs[i].join()
+
 
 DB.close()
